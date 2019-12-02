faa8623e240596de37f1f2939a93cc6bc89f2dbd
qq741263802 <741263802@qq.com>
2019-12-02 23:29:00 +0800

    333333

--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -5,22 +5,29 @@ import java.io.IOException;
 
 public class Logger {
 
+    private String name;
+    public  int price;
+
 //写入日志类
     public void settext() throws IOException {
         File f =new File("log.txt");
-        if(f.exists())
-        {
+        if(f.exists()) {
 
             f.createNewFile();
 
         }
 
-
-
-
     }
 
 
+    public   Logger(String name)
+    {
+
+        this.name=name;
+        System.out.println(this.name);
+        System.out.println(price);
+
+    }
 
 
 }
